e6fe71765df6b2fa562ef3d11b0a35e962c4d633
wafa200099 <wafaa.alomour@gmail.com>
2022-10-16 13:56:26 +0300

    add commit

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,6 +1,7 @@
 import React,{useState,useEffect} from 'react'
 import './App.css';
-import { FaTrashAlt,FaPlusCircle } from "react-icons/fa";
+import { FaTrashAlt,FaPlusCircle,FaCheck } from "react-icons/fa";
+
 const getLocalStorage=()=>{
   let myLocalTasks = localStorage.getItem('todos')
   console.log( myLocalTasks);
@@ -13,14 +14,15 @@ const getLocalStorage=()=>{
 function App() {
   const [list,setList]=useState(getLocalStorage())
   const [input,setInput]=useState("")
-
+  const [done,setDone]=useState(false)
 
   const addToDo =(todo)=>{
 
-      const newToDo={
-        id:Math.random(), // to identify the todo
-        todo:todo,
-       }
+    const newToDo={
+      id:Math.random(), // to identify the todo
+      todo:todo,
+      isDone:false
+     }
        setList([...list,newToDo])
 // add todo to list
 // add a new todo to the exisiting element in list
@@ -28,6 +30,24 @@ function App() {
 //clear input 
   setInput(" ")
   }
+
+  const doneToDo = (id) => {
+    console.log(id);
+   const newList =list.filter((todo)=>todo.id === id)
+    if(newList){
+     setDone(!done)
+   }
+  }
+  // const doneToDo = id => {
+  //   const [done,setDone]=useState(false)
+  //   const newTodos = [...list];
+  //   console.log(list);
+  //   newTodos[id].isDone = true;
+  //   setList(newTodos);
+  // };
+
+
+
   const deleteToDo=(id)=>{
    const newList=list.filter((todo)=>todo.id !== id)
    setList(newList)
@@ -37,10 +57,6 @@ function App() {
     localStorage.setItem("todos", JSON.stringify(list));
   }, [list]);
 
-  useEffect(()=>{
-    let myLocalTasks = JSON.parse(window.localStorage.getItem('todos'))
-    setList(myLocalTasks);
-    },[]);
 
 
 
@@ -57,8 +73,10 @@ function App() {
        <ul style={{listStyle:"none" }}>
         {list.map((todo)=>(
         <li style={{padding:'5px',marginTop:"20px"}}  key={todo.id}>
-       {todo.todo} 
+       {/* {todo.todo}  */}
+        <span style={{ textDecoration: done ? "line-through" : "" }}>{todo.todo}</span>
         <button onClick={()=>deleteToDo(todo.id)} style={{marginLeft:'100px',padding:'7px'}}><FaTrashAlt/></button>
+        <button onClick={()=>doneToDo(todo.id)} style={{marginLeft:'10px',padding:'7px'}}><FaCheck/></button>
         </li>
       ))}
        </ul>
@@ -67,3 +85,99 @@ function App() {
 }
 
 export default App
+
+// import React from "react";
+// import "./App.css";
+// import { Button, Card, Form } from 'react-bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+// function Todo({ todo, index, markTodo, removeTodo }) {
+//   return (
+//     <div
+//       className="todo"
+      
+//     >
+//       <span style={{ textDecoration: todo.isDone ? "line-through" : "" }}>{todo.text}</span>
+//       <div>
+//         <Button variant="outline-success" onClick={() => markTodo(index)}>✓</Button>{' '}
+//         <Button variant="outline-danger" onClick={() => removeTodo(index)}>✕</Button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// function FormTodo({ addTodo }) {
+//   const [value, setValue] = React.useState("");
+
+//   const handleSubmit = e => {
+//     e.preventDefault();
+//     if (!value) return;
+//     addTodo(value);
+//     setValue("");
+//   };
+
+//   return (
+//     <Form onSubmit={handleSubmit}> 
+//     <Form.Group>
+//       <Form.Label><b>Add Todo</b></Form.Label>
+//       <Form.Control type="text" className="input" value={value} onChange={e => setValue(e.target.value)} placeholder="Add new todo" />
+//     </Form.Group>
+//     <Button variant="primary mb-3" type="submit">
+//       Submit
+//     </Button>
+//   </Form>
+//   );
+// }
+
+// function App() {
+//   const [todos, setTodos] = React.useState([
+//     {
+//       text: "This is a sampe todo",
+//       isDone: false
+//     }
+//   ]);
+
+//   const addTodo = text => {
+//     const newTodos = [...todos, { text }];
+//     setTodos(newTodos);
+//   };
+
+//   const markTodo = index => {
+//     const newTodos = [...todos];
+//     newTodos[index].isDone = true;
+//     setTodos(newTodos);
+//   };
+
+//   const removeTodo = index => {
+//     const newTodos = [...todos];
+//     newTodos.splice(index, 1);
+//     setTodos(newTodos);
+//   };
+
+//   return (
+//     <div className="app">
+//       <div className="container">
+//         <h1 className="text-center mb-4">Todo List</h1>
+//         <FormTodo addTodo={addTodo} />
+//         <div>
+//           {todos.map((todo, index) => (
+//             <Card>
+//               <Card.Body>
+//                 <Todo
+//                 key={index}
+//                 index={index}
+//                 todo={todo}
+//                 markTodo={markTodo}
+//                 removeTodo={removeTodo}
+//                 />
+//               </Card.Body>
+//             </Card>
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
